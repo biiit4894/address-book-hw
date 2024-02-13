9e2770dac94bc22b5692e1cf2fea8de8c40765f8
biiit4894 <ghiesf@naver.com>
2024-02-13 18:25:41 +0900

    Update: Scanner 자동 닫기 적용

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -6,35 +6,36 @@ public class AddressBook {
     private static List<Contact> contactList = new ArrayList<>();
 
     public void start() {
-        Scanner sc = new Scanner(System.in);
-        while(true) {
-            System.out.println("---------------------");
-            System.out.println("1. 비즈니스 연락처 추가");
-            System.out.println("2. 개인 연락처 추가");
-            System.out.println("3. 연락처 출력");
-            System.out.println("4. 연락처 검색");
-            System.out.println("5. 종료");
-            System.out.println("---------------------");
-            System.out.print("메뉴를 선택해주세요: ");
-            String menu = sc.next();
-            switch(menu) {
-                case("1"):
-                    createBusinessContact(sc);
-                    break;
-                case("2"):
-                    createPersonalContact(sc);
-                    break;
-                case("3"):
-                    displayContacts();
-                    break;
-                case("4"):
-                    searchContacts(sc);
-                    break;
-                case("5"):
-                    System.out.println("프로그램을 종료합니다.");
-                    System.exit(0);
-                default:
-                    System.out.println("잘못된 메뉴를 입력하셨습니다.");
+        try(Scanner sc = new Scanner(System.in)) {
+            while(true) {
+                System.out.println("---------------------");
+                System.out.println("1. 비즈니스 연락처 추가");
+                System.out.println("2. 개인 연락처 추가");
+                System.out.println("3. 연락처 출력");
+                System.out.println("4. 연락처 검색");
+                System.out.println("5. 종료");
+                System.out.println("---------------------");
+                System.out.print("메뉴를 선택해주세요: ");
+                String menu = sc.next();
+                switch(menu) {
+                    case("1"):
+                        createBusinessContact(sc);
+                        break;
+                    case("2"):
+                        createPersonalContact(sc);
+                        break;
+                    case("3"):
+                        displayContacts();
+                        break;
+                    case("4"):
+                        searchContacts(sc);
+                        break;
+                    case("5"):
+                        System.out.println("프로그램을 종료합니다.");
+                        System.exit(0);
+                    default:
+                        System.out.println("잘못된 메뉴를 입력하셨습니다.");
+                }
             }
         }
     }
